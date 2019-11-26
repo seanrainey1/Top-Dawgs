8c5e7403b77ea21fefd095759aa377131f2987c0
Sean Rainey <sean.rainey.1@ou.edu>
2019-11-26 16:48:16 -0600

    added broken computer list function

--- seatAssigner/seatAssigner/MainWindow.xaml.cs
+++ seatAssigner/seatAssigner/MainWindow.xaml.cs
@@ -28,20 +28,33 @@ namespace seatAssigner
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            int numStudents = 0;
+         
             Random rand = new Random();
           
 
-            if (File.Exists(fileReadTXT.Text)== true)
+            if (File.Exists(fileReadTXT.Text)== true && File.Exists(brokeCompsTXT.Text) == true)
             {
-                
-                var lines = File.ReadAllLines(fileReadTXT.Text);
+
+
+                var lines = File.ReadAllLines(fileReadTXT.Text); //reads in each line of the student name csv
 
                 var first_name = string.Empty;
                 var last_name = string.Empty;
-                List<int> usedSeats = new List<int>();
-                
-              
+
+                List<string> usedSeats = new List<string>(); //creates list to hold seat numbers which have already been used
+
+                var values = File.ReadAllLines(brokeCompsTXT.Text); //reads in each line of broken computer csv
+
+                var comps = string.Empty;
+
+                List<string> brokeComps = new List<string>(); // creates list to hold the broken computer csv values
+
+
+
+                for (int i = 1; i < values.Length; i++) //adds all broken computer numbers to the brokecomps list
+                {
+                    brokeComps.Add(comps);
+                }
 
                 for (int i = 1; i < lines.Length; i++)
                 {
@@ -50,17 +63,17 @@ namespace seatAssigner
                     first_name = column[0];
                     last_name = column[1];
                     int assignedSeat = rand.Next(50);
-                    
-                   
-                    if(usedSeats.Contains(assignedSeat))
+                  
+
+                    if (usedSeats.Contains(Convert.ToString(assignedSeat)) || usedSeats.Contains(Convert.ToString(brokeComps)))
                     {
                         do
                         {
                             assignedSeat = rand.Next(50);
-                        } while (usedSeats.Contains(assignedSeat) == true);
+                        } while (usedSeats.Contains(Convert.ToString(assignedSeat)) || usedSeats.Contains(Convert.ToString(brokeComps)));
                     }
 
-                    usedSeats.Add(assignedSeat);
+                    usedSeats.Add(Convert.ToString(assignedSeat));
 
                     assignedSeatsLB.Items.Add($"{first_name} {last_name} seat {assignedSeat}");
                 }
@@ -77,6 +90,13 @@ namespace seatAssigner
             var result = dlg.ShowDialog();
             fileReadTXT.Text = dlg.FileName;
             
+        }
+
+        private void ReadWorkstationsBut_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            var result = dlg.ShowDialog();
+            brokeCompsTXT.Text = dlg.FileName;
         }
     }
 }
